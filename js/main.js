30bb43c368037bb90f407a03d9629c2fc817dbf1
alex zhang <alexzhangh@gmail.com>
2012-12-10 02:24:48 -0800

    move js to separate file for main mockup index, give pricing box a full width banner

---
+++ js/main.js
@@ -0,0 +1,48 @@
+         $(document).ready(function() {
+		     
+	var $win = $(window);
+	var visible = true;
+	processScroll()
+    $win.on('scroll', processScroll);
+	$('#dismiss').on('click',function(e) {e.preventDefault(); $('.fixed-footer').fadeOut(200,function() {$(this).css({"visibility":"hidden"});});});
+	var height = $(document).height() / 2;
+    function processScroll() {
+      var i, scrollTop = $(window).scrollTop()
+      if (scrollTop >= height && visible) {
+			$('.fixed-footer').fadeOut();
+			visible = false;
+	  } else {
+	  if(scrollTop < height && !visible) {
+		  $('.fixed-footer').fadeIn();
+		  visible = true;
+	  }
+	  }
+    }
+         	$('blockquote').quovolver();
+         	$('#calendar').fullCalendar({
+         contentHeight:600,
+         eventBackgroundColor:"#880000",
+         eventBorderColor:"#880000",
+         		events: {
+                    		url: 'https://www.google.com/calendar/feeds/0d43qb2uu4qv0gvtidv7g2revo%40group.calendar.google.com/public/basic',
+                   		editable:false
+               	 		},
+         		
+         		eventClick: function(event) {
+         			// opens events in a popup window
+         			window.open(event.url, 'gcalevent', 'width=700,height=600');
+         			return false;
+         		},
+         		
+         		loading: function(bool) {
+         			if (bool) {
+         				$('#loading').show();
+         			}else{
+         				$('#loading').hide();
+         			}
+         		},
+         		editable: true
+         		
+         	});
+         	
+         });
